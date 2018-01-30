41c97d1a64c60a1a81d38f3a6397ff5ce00851c0
Mikhail Krainik <mikhail.krainik@gmail.com>
2018-01-30 17:46:14 +0200

    add english version

--- index.rb
+++ index.rb
@@ -6,13 +6,13 @@ require 'psych'
 require 'erb'
 
 MainLocale = :ua
-Suffix = "demo_"
+Suffix = ""
 TemplateName = 'index.erb'
 LocalizationFile = 'index.yaml'
 
 data = Psych.safe_load(IO.read(LocalizationFile), symbolize_names: true)
 content = IO.read(TemplateName)
-[MainLocale, :us].each do |locale|
+[MainLocale, :en].each do |locale|
   IO.write("#{Suffix}index_#{locale}.html", ERB.new(content).result_with_hash(c: data[locale]))
 end
 
